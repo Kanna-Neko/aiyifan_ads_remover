f2566cf4d8463d323b7fcb227a848e965ee695a7
KannaNeko <54601297+Kanna-Neko@users.noreply.github.com>
2024-11-30 18:40:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Aiyifan_ad_remover
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/Kanna-Neko/aiyifan_ads_remover
 // @version      2024-11-30
 // @description  try to take over the world!
-// @author       You
+// @author       Kannaneko
 // @match        https://www.yfsp.tv/play/**
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yfsp.tv
 // @grant        none
